b13b0a08c7140a3b3b161c01b6083240e096c011
CassiaY <137340466+CassiaY@users.noreply.github.com>
2023-10-05 22:49:50 -0500

    commented out unfinished code to avoid error

--- static/js/app.js
+++ static/js/app.js
@@ -5,22 +5,22 @@ d3.json(url).then(function(data) {
 });
 
 // create horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
-let title = 'Belly Button Biodiversity: OTU Species Colonies'
-let sample_values = []//bar chart values data.samples
-let otu_ids = [] //bar chart labels 
-let otu_labels = [] //hovertext for the chart
+// let title = 'Belly Button Biodiversity: OTU Species Colonies'
+// let sample_values = []//bar chart values
+// let otu_ids = [] //bar chart labels 
+// let otu_labels = [] //hovertext for the chart
 
-let trace1 = {
-  x: ,
-  y: ,
-  type: 'bar',
-  orientation: 'h'
-};
+// let trace1 = {
+//   x: ,
+//   y: ,
+//   type: 'bar',
+//   orientation: 'h'
+// };
 
-let data = [trace1];
+// let data = [trace1];
 
-let layout = {
-  title: title
-};
+// let layout = {
+//   title: title
+// };
 
-Plotly.newPlot("plot", data, layout);
+// Plotly.newPlot("plot", data, layout);
